c57bfb000491220539e972b0aaf360e3cc05844d
Arman Siddiqui <arman@golokait.com>
2025-01-05 16:18:18 +0530

    chnaged db pointing

--- constant.jsx
+++ constant.jsx
@@ -1,5 +1,5 @@
 // export const BASE_URL = "https://server.neeljewel.com:8443/wep-0.0.1-SNAPSHOT";
 // export const BASE_URL = "http://3.111.12.44:8080/wep-0.0.1-SNAPSHOT";
-// export const BASE_URL = "http://65.0.97.239:8080/wep-0.0.1-SNAPSHOT";
-// export const BASE_URL = "http://127.0.0.1:8081";
-export const BASE_URL = "https://devoted-energy-production.up.railway.app";
+export const BASE_URL = "https://jewelicious-server-production.up.railway.app";
+// export const BASE_URL = "http://127.0.0.1:8080";
+// export const BASE_URL = "https://devoted-energy-production.up.railway.app";
